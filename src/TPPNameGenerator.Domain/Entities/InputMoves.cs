bc36f1418585779b4d3a9f0f479889449368d5a1
Gilles <Gilles@DESKTOP>
2025-08-25 14:53:05 +0200

    Setup skeleton project

---
+++ src/TPPNameGenerator.Domain/Entities/InputMoves.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPPNameGenerator.Domain.Entities
+{
+    public enum InputMoves
+    {
+        A, //Select letter
+        B, // remove last letter
+
+        Up,
+        Down,
+        Left,
+        Right,
+
+        Start, // go to End button
+        Select //Swap keyboards
+    }
+}
---
+++ src/TPPNameGenerator.Domain/Entities/VirtualKeyboard.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPPNameGenerator.Domain.Entities
+{
+    /// <summary>
+    /// A clear, consise representation of the virtual keyboard layouts used in the naming screen.
+    /// </summary>
+    /// <remarks
+    /// Yes, this could be automatically generated, but this shorter and more readable in my opinion. 
+    ///</remarks>
+
+    public static class VirtualKeyboard
+    {
+        public static string[,] UppercaseKeyboardDefault =
+        {
+            { "A", "B", "C", "D", "E", "F", " ", ".", "SWAP" },
+            { "G", "H", "I", "J", "K", "L", " ", ",", "BACK" },
+            { "M", "N", "O", "P", "Q", "R", "S", " ", "BACK" },
+            { "T", "U", "V", "W", "X", "Y", "Z", " ", "OK"  }  
+        };
+
+        public static string[,] LowercaseKeyboard =
+        {
+            { "a", "b", "c", "d", "e", "f", " ", "." , "SWAP" },
+            { "g", "h", "i", "j", "k", "l", " ", "," , "BACK" },
+            { "m", "n", "o", "p", "q", "r", "s", " " , "BACK" },
+            { "t", "u", "v", "w", "x", "y", "z", " ", "OK" }
+        };
+
+        public static string[,] NumberSymbolKeyboard =
+        {
+            { "0", "1", "2", "3", "4", " " , "SWAP" },
+            { "5", "6", "7", "8", "9", " " , "BACK" },
+            { "!", "?", "♂", "♀", "/", "-" , "BACK" },
+            { "…", "“", "”", "‘", "’", " ", "OK" }
+        };
+    }
+}
